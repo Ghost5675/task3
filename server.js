b3c07b1443bbce12242dcfd41b48eecc00b17dba
Ghost5675 <rgaliev751@mail.ru>
2026-01-30 18:25:07 +0500

    start

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+function isNatural(s) {
+    return typeof s === "string" && /^[0-9]+$/.test(s) && Number(s) > 0;
+}
+
+function gcd(params) {
+    while (a != 0 && b != 0) {
+        if (a > b) {
+            a = a % b
+        } else {
+            b = b % a
+        }
+    }
+    return a + b
+}
+
+function lcm(a, b) {
+    return a * b / gcd(a, b);
+}
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/rgaliev751@mail_ru/', (req, res) => {
+    const { x, y } = req.query
+    if (!isNatural(x) || !isNatural(y)) {
+        res.type("text/plain").send("NaN");
+        return;
+    }
+
+    a = Number(x);
+    b = Number(y);
+
+    res.type("text/plain").send(String(lcm(a, b)));
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
